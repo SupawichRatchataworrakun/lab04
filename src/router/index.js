4555de2ae23c1489ce79719d4f2c26fcc8867a6c
supawich_r <supawich_r@elearning.cmu.ac.th>
2021-07-23 01:30:08 +0700

    3.2

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import About from '../views/About.vue'
 import EventDetails from '@/views/event/Details.vue'
 import EventRegister from '@/views/event/Register.vue'
 import EventEdit from '@/views/event/Edit.vue'
+import EventLayout from '@/views/event/Layout.vue'
 
 const routes = [
   {
@@ -23,21 +24,28 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
-  },
-  {
-    path: '/event/:id/register',
-    name: 'EventRegister',
-    props: true,
-    component: EventRegister
-  },
-  {
-    path: '/event/:id/edit',
-    name: 'EventEdit',
-    props: true,
-    component: EventEdit
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        props: true,
+        component: EventEdit
+      }
+    ]
   }
 ]
 
